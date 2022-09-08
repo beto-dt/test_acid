9560018cb01ad734902e4750ace7eaba50ba82bb
beto-dt-21 <luis.delatorre@kinanalytics.com>
2022-09-08 16:52:44 -0500

    feat:add push in the controller purchases

--- controllers/purchases.js
+++ controllers/purchases.js
@@ -1,5 +1,6 @@
-const { purchasesModel } = require('../models');
+const { purchasesModel, productsModel, usersModel } = require('../models');
 const handleHttpError = require('../utils/handleError');
+const { QueryTypes } = require('sequelize');
 
 /**
  * Get List of all the Purchases
@@ -42,8 +43,18 @@ const getPurchase = async (req, res) => {
 const createPurchase = async (req, res) => {
     try{
         const body = req.body;
-        const data = await purchasesModel.create(body);
-        res.send({data});
+        const user_id = await usersModel.findOne({where: {id:body.user_id}})
+        const product_id = await productsModel.findOne({where: {id:body.products_id}})
+        if(user_id == null){
+            handleHttpError(res,"NOT_USER_EXITS")
+        }else{
+            if( product_id == null ){
+                handleHttpError(res,"NOT_PRODUCT_EXITS")
+            }else{
+                const data = await purchasesModel.create(body);
+                res.send({data});
+            }
+        }
     }catch(e){
         handleHttpError(res,"ERROR_POST_PURCHASE")
     }
@@ -58,8 +69,18 @@ const updatePurchase = async (req, res) => {
     try{
         const body = req.body;
         const id = req.params.id;
-        const data = await purchasesModel.update(body,{ where: { id: id}});
-        res.send({data});
+        const user_id = await usersModel.findOne({where: {id:body.user_id}})
+        const product_id = await productsModel.findOne({where: {id:body.products_id}})
+        if(user_id == null){
+            handleHttpError(res,"NOT_USER_EXITS")
+        }else{
+            if( product_id == null ){
+                handleHttpError(res,"NOT_PRODUCT_EXITS")
+            }else{
+                const data = await purchasesModel.update(body,{ where: { id: id}});
+                res.send({data});
+            }
+        }
     }catch(e){
         handleHttpError(res,"ERROR_PUT_PURCHASE")
     }
@@ -80,5 +101,30 @@ const deletePurchase = async (req, res) => {
     }
 }
 
+/**
+ * Total of Purchases per user
+ * @param {*} req
+ * @param {*} res
+ */
+const totalPurchasesPerUser = async (req, res) => {
+    try{
+        const user_id = req.params.id;
+        const data = await purchasesModel.sequelize.query(`SELECT concat( users.name,'', users.lastname) as cliente, products.name , products.price FROM users INNER JOIN purchases ON users.id = purchases.user_id INNER JOIN products ON purchases.products_id = products.id WHERE users.id = ${user_id} `,{ type: QueryTypes.SELECT })
+        res.send({data})
+    }catch(e){
+        handleHttpError(res,"ERROR_TOTAL_PER_PURCHASE")
+    }
+}
 
-module.exports = { getPurchases, getPurchase, createPurchase, updatePurchase, deletePurchase };
+const totalPurchasesPerPayUser = async (req, res) => {
+    try{
+        const user_id = req.params.id;
+        const data = await purchasesModel.sequelize.query(`SELECT SUM(products.price) as Total FROM users INNER JOIN purchases ON users.id = purchases.user_id INNER JOIN products ON purchases.products_id = products.id WHERE users.id = ${user_id}`,{ type: QueryTypes.SELECT })
+        res.send({data})
+    }catch(e){
+        handleHttpError(res,"ERROR_TOTAL_PER_PAY_PURCHASE")
+    }
+}
+
+
+module.exports = { getPurchases, getPurchase, createPurchase, updatePurchase, deletePurchase,totalPurchasesPerUser,totalPurchasesPerPayUser };
--- routes/purchases.js
+++ routes/purchases.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { getPurchases, getPurchase, createPurchase, updatePurchase, deletePurchase } = require("../controllers/purchases");
+const { getPurchases, getPurchase, createPurchase, updatePurchase, deletePurchase, totalPurchasesPerUser, totalPurchasesPerPayUser } = require("../controllers/purchases");
 const router = express.Router();
 
 router.get("/",getPurchases);
 router.get("/:id",getPurchase);
+router.get("/users/:id",totalPurchasesPerUser);
+router.get("/total/:id",totalPurchasesPerPayUser);
 router.post("/",createPurchase);
 router.put("/:id",updatePurchase);
 router.delete("/:id",deletePurchase);
